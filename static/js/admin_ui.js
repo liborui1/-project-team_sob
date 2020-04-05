6ba41bbc9f0462a1865f9daf70a8fb387b355421
SohailAhmed10 <ssyed.ahmed@mail.utoronto.ca>
2020-04-05 05:10:48 -0400

    Added code for showing users but not working

--- static/js/admin_ui.js
+++ static/js/admin_ui.js
@@ -13,6 +13,25 @@
             var popup = document.getElementById("myPopup");
             popup.classList.toggle("show");
             });
+
+        api.onConnectedUserUpdate(function(users){
+            document.getElementById('myPopup').innerHTML = '';
+            for (let peerid in users) {
+                let username = users[peerid];
+                let cmnt_element = document.createElement('div');
+                cmnt_element.className = "user";
+                cmnt_element.innerHTML = `
+                ${username}
+                <div>
+                        <button>Kick</button>
+                        <button>Allow to draw</button>
+                    </div>
+                `;
+
+                document.querySelector('#myPopup').prepend(cmnt_element);
+            }
+        });
+
         
         
     });
